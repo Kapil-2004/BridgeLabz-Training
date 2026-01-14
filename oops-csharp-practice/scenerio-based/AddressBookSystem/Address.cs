19675065b3150a42dd136058bd911ab2be4bebea
Kapil-2004 <kapil202004@gmail.com>
2026-01-14 12:20:29 +0530

    [KapilGautam] getter-setter for address

---
+++ oops-csharp-practice/scenerio-based/AddressBookSystem/Address.cs
@@ -0,0 +1,81 @@
+using System;
+
+namespace AddressBookSystem
+{
+    public class Address
+    {
+        private string firstName;
+        private string lastName;
+        private string address;
+        private string city;
+        private string state;
+        private string zip;
+        private string phoneNumber;
+        private string email;
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        public string City
+        {
+            get { return city; }
+            set { city = value; }
+        }
+
+        public string State
+        {
+            get { return state; }
+            set { state = value; }
+        }
+
+        public string Zip
+        {
+            get { return zip; }
+            set { zip = value; }
+        }
+
+        public string PhoneNumber
+        {
+            get { return phoneNumber; }
+            set { phoneNumber = value; }
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        public Address(string firstName, string lastName, string address, string city, string state, string zip, string phoneNumber, string email)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.zip = zip;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+        }
+
+        public string GetDetails()
+        {
+            return $"{firstName} {lastName}, {address}, {city}, {state}, {zip}, {phoneNumber}, {email}";
+        }
+    }
+}
